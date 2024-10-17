1fc233e179ed76ec243791f113ac36f82caffdcd
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-10-17 16:41:19 +0530

    add

--- src/assets/Orders.jsx
+++ src/assets/Orders.jsx
@@ -18,14 +18,74 @@ const Orders = () => {
   const [totalQty, setTotalQty] = useState("")
   const [userDetails, setUserDetails] = useState({})
   const [amountDetailsToggle, setAmountDetailsToggle] = useState("")
-
+  const [submitSpin, setSubmitSpin] = useState(false)
+  const [trackNum, setTrackNum] = useState("")
+  const [mailCard, setMailCard] = useState(false)
 
   // send mail function
-
   const sendMail = (orderId) => {
     const user = orders.find((item) => item._id === orderId)
     setUserDetails(user)
+    setMailCard(true)
   }
+
+
+  const formData = {
+    to: userDetails.email,
+    subject: "ISKCON YANAM STORES - Order Shipped Successfully",
+    html: `
+      <h2>Dear ${userDetails.fullName},</h2>
+      <p>We are delighted to inform you that your order from <strong>ISKCON YANAM STORES</strong> has been successfully shipped!</p>
+      
+      <h3>Customer Details:</h3>
+      <ul>
+        <li><strong>Customer Name:</strong> ${userDetails.fullName}</li>
+        <li><strong>Customer Email:</strong> ${userDetails.email}</li>
+      
+      </ul>
+  
+      <h3>Shipping Address:</h3>
+      <p>
+        ${userDetails.city},<br>
+        ${userDetails.address}, ${userDetails.state} - ${userDetails.pin},<br>
+      
+      </p>
+  
+      <p>You can track your order on our website using the tracking number below by visiting the following link:</p>
+        
+      <h4><strong>Tracking Number:</strong> ${trackNum}</h4>
+      
+      <p><a href="https://example.com">Track Your Order Here</a></p>
+  
+      <p>Thank you for shopping with us, and we hope you enjoy your purchase!</p>
+      <br>
+      <p>Sincerely,</p>
+      <p><strong>ISKCON YANAM STORES</strong></p>
+    `,
+  };
+
+
+  // sending mail function
+  const submitFunc = async () => {
+    setSubmitSpin(true);
+    try {
+      const res = await axios.post(`${api}/mail/sendmail`, formData);
+      if (res) {
+        toast.success(
+          "Email has been Sent Successfully."
+        );
+        setSubmitSpin(false);
+        setMailCard(false)
+
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Please try again");
+      setSubmitSpin(false);
+    }
+  };
+
+
 
   //Calculating total Amount function logic
   const getTotalAmount = (orderId) => {
@@ -180,11 +240,14 @@ const Orders = () => {
                           <h2 className="font-semibold mb-3">
                             PAYMENT RECEIPT
                           </h2>
-                          <img
-                            src={item.paymentScreenShot}
-                            alt="receipt"
-                            className="mt-5 h-52 w-52 rounded"
-                          />
+                          <div className="h-52 overflow-y-auto">
+
+                            <img
+                              src={item.paymentScreenShot}
+                              alt="receipt"
+                              className="mt-5 w-52 rounded"
+                            />
+                          </div>
                           {amountDetailsToggle === item._id ? <h3 className="font-semibold mt-3 cost-details ">
                             TOTAL COST :
                             <span className="text-black pl-1">₹{totalPrice}</span>
@@ -276,6 +339,25 @@ const Orders = () => {
           )}
         </div>
       )}
+
+      {/* mail card  */}
+      {mailCard &&
+
+        <div onClick={() => setMailCard(false)} className="fixed top-0 left-0 px-3 bg-gray-700 o bg-opacity-50 w-screen h-screen flex justify-center items-center">
+          <div onClick={(e) => e.stopPropagation()} className="bg-white p-5 rounded w-[20rem]">
+            <h5 className="mb-2 text-lg font-semibold">Track Number</h5>
+            <input type="text" className="pl-4 h-10 w-full mb-3 outline-none rounded-full border-2 border-orange-600" name="trackNumn" value={trackNum} onChange={(e) => setTrackNum(e.target.value)} />
+            {submitSpin ?
+              <button className="bg-blue-700 text-white h-8 w-[8rem] rounded-full">Sending...</button>
+              :
+              <button onClick={submitFunc} className="bg-blue-700 text-white h-8 w-[8rem] rounded-full">Send Mail</button>
+            }
+            <button onClick={() => setMailCard(false)} className="bg-gray-600 ml-2 text-white h-8 w-[6rem] rounded-full">Close</button>
+
+          </div>
+        </div>
+      }
+
     </>
   );
 };
